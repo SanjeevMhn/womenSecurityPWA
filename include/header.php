48d52c98dd704ae13d56944c0b32593d2cbc9b44
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2022-04-30 23:29:55 +0545

    [ADDED]:added text content to the site

--- include/header.php
+++ include/header.php
@@ -8,3 +8,30 @@
     <link rel="stylesheet" href="./dist/css/style.min.css">
 </head>
 <body>
+    <nav class="primary-nav">
+        <h2 class="brand">
+            WS
+        </h2>
+        <ul class="nav-list">
+            <li class="nav-list__item">
+                <a href="" class="nav-list__link">
+                    Home
+                </a>
+            </li>
+            <li class="nav-list__item">
+                <a href="" class="nav-list__link">
+                    About
+                </a>
+            </li>
+            <li class="nav-list__item">
+                <a href="" class="nav-list__link">
+                    Register
+                </a>
+            </li>
+            <li class="nav-list__item">
+                <a href="" class="nav-list__link">
+                    Login
+                </a>
+            </li>
+        </ul>
+    </nav>
--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php include_once "./include/header.php" ?>
-   <h1>Hello World</h1> 
+   <div class="banner">
+      <div class="container">
+         <h1>Women Security App</h1> 
+         <p>Report for cases such as harrasment, abuse both online and real life</p>
+      </div>
+   </div>
+   <div class="features">
+      <div class="container">
+         <ul class="features-list">
+            <li class="features-list__item">
+               24 hrs, support.
+            </li>
+            <li class="features-list__item">
+               Dedicated personnel on standby.
+            </li>
+            <li class="features-list__item">
+               Immediate action within 10 minutes of case filed.
+            </li>
+            <li class="features-list__item">
+               Send messages to guardians through double click of the power button (on mobile devices only).
+            </li>
+            <li class="features-list__item">
+               100% privacy.
+            </li>
+         </ul>
+      </div>
+   </div>
+   <div class="project-info">
+      <div class="container">
+         <p>
+            Women and young girls are constantly victim of crimes. Although its sad we cannot prevent every one of those crimes, we can do our part by reporting such crimes.
+            If we want to create an environment which is safe for women and girls, we need to act on it now by reporting those crimes so that those who are the victims get justice as quick as possible.
+            Abuse of any kind is never to be tolorated, fighting for what is right matters.
+         </p>
+      </div>
+   </div>
 
 <?php include_once "./include/footer.php" ?>
